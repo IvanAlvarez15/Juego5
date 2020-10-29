435eba73b474e114eb41c20b7670a9bfedd048ba
A01655181 <73494815+A01655181@users.noreply.github.com>
2020-10-29 09:07:31 -0600

    cambios a los digitos, color de digitos y la imagen de fondo
    Este commit modifica los dígitos, su color y la imagen de fondo

---
+++ Juego5_cambio_de_color_y_imagen.py
@@ -0,0 +1,93 @@
+from random import *
+from turtle import *
+from freegames import path
+
+
+contar = 1
+lil_turtle = path('Turtle_walk.gif')
+tiles = list(range(32)) * 2
+state = {'mark': None}
+hide = [True] * 64
+
+def square(x, y):
+    "Draw white square with black outline at (x, y)."
+    up()
+    goto(x, y)
+    down()
+    color('black', 'white')
+    begin_fill()
+    for count in range(4):
+        forward(50)
+        left(90)
+    end_fill()
+
+def index(x, y):
+    "Convert (x, y) coordinates to tiles index."
+    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+
+def xy(count):
+    "Convert tiles count to (x, y) coordinates."
+    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+
+def tap(x, y):
+    "Update mark and hidden tiles based on tap."
+    spot = index(x, y)
+    mark = state['mark']
+    global contar
+    
+    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
+        state['mark'] = spot
+        contar+=1 # Suma uno al tap
+
+
+    else:
+        hide[spot] = False
+        hide[mark] = False
+        state['mark'] = None
+
+def draw():
+    "Draw image and tiles."
+    clear()
+    goto(0, 0)
+    shape(lil_turtle)
+    stamp()
+    for count in range(64):
+        if hide[count]:
+            x, y = xy(count)
+            square(x, y)
+    mark = state['mark']
+
+    if mark is not None and hide[mark]:
+        x, y = xy(mark)
+        up()
+        goto(x + 25, y+1)
+        color('orange')
+        write(tiles[mark], font=('Arial', 30, 'normal'), align="Center")
+    
+    
+    up()
+    goto(-180,230)
+    write(contar, font=('Arial', 15, 'normal'), align="Center") 
+
+    update()
+    ontimer(draw, 100)
+
+
+
+
+
+
+
+
+
+
+
+
+shuffle(tiles)
+setup(420, 520, 370, 0)
+addshape(lil_turtle)
+hideturtle()
+tracer(False)
+onscreenclick(tap)
+draw()
+done()
